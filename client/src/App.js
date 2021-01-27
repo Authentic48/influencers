eb0b59d1a5e47f9a1996c7af9cfb77ede6485cb9
MohamedAhmed122 <58720977+MohamedAhmed122@users.noreply.github.com>
2021-01-27 13:35:54 +0700

    Adding all the needed Style before Starting

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,12 @@ function App() {
   return (
     <div className="App">
      <h1>Hello world</h1>
+     <button className='btn_main'>
+       hello it is me
+     </button>
+     <button className='btn_secondary'>
+       hello it is me
+     </button>
     </div>
   );
 }
