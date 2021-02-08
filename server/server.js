f7df0c76e697c22fba772d19123de26638b087d7
Authentic48 <iboufall48@gmail.com>
2021-02-08 08:19:44 +0700

    db connection setup

--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
 import express from 'express';
+import connectDB from './Config/db.js'
+import dotenv from 'dotenv'
+
+connectDB()
 
 const app = express()
+
+connectDB()
 
 app.listen(5000, console.log('app is running on port 5000'))
