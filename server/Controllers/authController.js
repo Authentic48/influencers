715589b68898491546989cb923a61be27cc7b3af
Authentic48 <iboufall48@gmail.com>
2021-02-08 11:21:06 +0700

    registration done

--- server/Controllers/authController.js
+++ server/Controllers/authController.js
@@ -43,7 +43,7 @@ export const registerUser = asyncHandler (async (req, res) =>{
    const user = await User.create({
       name,
       email,
-      password
+      password,
    })
    if (user){
       res.status(201).json({
--- server/Routes/userRoute.js
+++ server/Routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { authUser, getUserProfile } from '../Controllers/authController.js'
+import { authUser, getUserProfile, registerUser } from '../Controllers/authController.js'
 import {protect} from '../middleware/authMiddleware.js'
 const router = express.Router()
 
 router.post('/login', authUser)
+router.route('/register').post(registerUser)
 
 router.route('/profile').get(protect, getUserProfile)
 
