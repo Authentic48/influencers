7ac1ef80f9ab1a08de4c1ed4cd110b891a052f6b
Authentic48 <iboufall48@gmail.com>
2021-02-10 20:57:06 +0700

    file upload done in the backend

--- server/Routes/uploadRoute.js
+++ server/Routes/uploadRoute.js
@@ -3,24 +3,30 @@ import aws from 'aws-sdk'
 import multer from 'multer'
 import multerS3 from  'multer-s3'
 import dotenv from 'dotenv'
+import path from 'path'
 
 dotenv.config()
 
 const router = express.Router()
-
 const bucket = process.env.DO_BUCKET
 const accessKeyId = process.env.ACCESS_KEY
-const secretAccessKey = process.env.ACCESS_SECRET
+const secretAccessKey = process.env.SECRET_KEY
 
-// Set S3 endpoint to DigitalOcean Spaces
-const spacesEndpoint = new aws.Endpoint('nyc3.digitaloceanspaces.com');
-const s3 = new aws.S3({
-  endpoint: spacesEndpoint,
+// Use our env vars for setting credentials. 
+aws.config.update({
   accessKeyId: accessKeyId,
-  secretAccessKey: secretAccessKey
+  secretAccessKey: secretAccessKey,
 });
 
-function checkFileType(file, cb) {
+// Set S3 endpoint to DigitalOcean Spaces
+const spacesEndpoint = new aws.Endpoint(process.env.DO_END_POINT);
+const s3 = new aws.S3({
+  endpoint: spacesEndpoint,
+  region: process.env.DO_DEFAULT_REGION
+});
+
+
+function checkFileType(file, cb) { 
   const filetypes = /jpg|jpeg|png/
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
   const mimetype = filetypes.test(file.mimetype)
@@ -31,13 +37,15 @@ function checkFileType(file, cb) {
     cb('Images only!')
   }
 }
+
+
 const storage = multerS3({
     s3: s3,
     bucket: bucket,
     acl: 'public-read',
     key: function(request, file, cb) {
-        console.log(file);
-        cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+        //console.log(file);
+        cb(null,  `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
     },   
 })
 const upload = multer({
@@ -48,7 +56,7 @@ const upload = multer({
 });
 
 router.post('/', upload.single('image'), (req, res) => {
-  res.send(`/${req.file.path}`)
+  res.send(`file upload successfully..`)
 })
 
 export default router
